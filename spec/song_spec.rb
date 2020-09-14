39257f817121d9aaa42f44365edafaa13e8b514f
noella callahan <calla147@gmail.com>
2020-09-14 03:45:08 +0000

    Automatically backed up by Learn

--- spec/song_spec.rb
+++ spec/song_spec.rb
@@ -21,47 +21,7 @@ describe 'Song' do
 
   describe '#artist=' do
     it 'sets the artist object to belong to the song' do
-      song = Song.new('Man in the Mirror')
-      new_artist_object = Artist.new('King of Pop')
-      song.artist = new_artist_object
-      expect(song.artist).to eq(new_artist_object)
-    end
-  end
-
-  describe '.all' do
-    it 'returns all existing Song instances' do
-      song = Song.new('Man in the Mirror')
-      expect(Song.all).to eq([song])
-      purple_rain = Song.new('Purple Rain')
-      expect(Song.all).to eq([song, purple_rain])
-    end
-  end
-
-  describe '.new_by_filename' do
-    it 'creates a new instance of a song from the file that\'s passed' do
-      file_name = 'Michael Jackson - Black or White - pop.mp3'
-      new_instance = Song.new_by_filename(file_name)
-      expect(new_instance.name).to eq('Black or White')
-    end
-
-    it 'associates new song instance with the artist from the filename' do
-      Artist.class_variable_set("@@all",[])
-      file_name = 'Michael Jackson - Black or White - pop.mp3'
-
-      new_instance = Song.new_by_filename(file_name)
-      expect(new_instance.artist.name).to eq('Michael Jackson')
-      expect(Artist.all.size).to eq(1)
-      expect(Artist.all.first.songs.empty?).to eq(false)
-    end
-  end
-
-  describe '#artist_name=' do
-    it "accepts an artist's name, finds or creates an Artist instance and assigns it to the Song's artist attribute" do
-      Artist.class_variable_set("@@all",[])
-
-      song = Song.new('Man in the Mirror')
-      song.artist_name = "Michael Jackson"
-      expect(song.artist).to be_an(Artist)
+      song = Song.new('Man in the Mi      expect(song.artist).to be_an(Artist)
       expect(song.artist.name).to eq("Michael Jackson")
 
       song_2 = Song.new('Thriller')
